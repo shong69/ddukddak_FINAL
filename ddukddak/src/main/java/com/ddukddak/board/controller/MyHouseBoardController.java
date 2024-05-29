081127cf8ed67ddcc31248c0a25a806a9f477a56
saemh95 <saem_hong_kr@hotmail.com>
2024-05-29 17:49:37 +0900

    240529/sh/커뮤니티 연결 + HTML 기초작업

---
+++ ddukddak/src/main/java/com/ddukddak/board/controller/MyHouseBoardController.java
@@ -0,0 +1,21 @@
+package com.ddukddak.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("myHouse")
+public class MyHouseBoardController {
+
+	@GetMapping("main")
+	public String myHouseMain() {
+		return "board/myHouseBoard/myHouseBoard";
+	}
+	
+	@GetMapping("detail")
+	public String myHouseDetail() {
+		return "board/myHouseBoard/myHouseBoardDetail";
+	}
+	
+}
