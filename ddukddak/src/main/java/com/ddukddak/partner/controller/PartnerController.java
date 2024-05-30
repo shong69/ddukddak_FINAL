bbdbc1b666d87394cc9135132e9ddf146406de75
Soowagger <soowagger@gmail.com>
2024-05-30 12:51:29 +0900

    240530 파트너 가입 작업중

--- ddukddak/src/main/java/com/ddukddak/partner/controller/PartnerController.java
+++ ddukddak/src/main/java/com/ddukddak/partner/controller/PartnerController.java
@@ -1,8 +1,10 @@
 package com.ddukddak.partner.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("partner")
@@ -12,6 +14,22 @@ public class PartnerController {
 	public String partnerLogin() {
 		
 		return "/partner/login";
+	}
+	
+	
+	/** 파트너 회원가입
+	 * @param choice : 1 : 시공사 / 2 : 판매사
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("signup")
+	public String partnerSignup(
+						@RequestParam(value = "choice", required = false) String choice, 
+						Model model) {
+		
+		model.addAttribute("choice", choice);
+		
+		return "partner/signup";
 	}
 	
 }
