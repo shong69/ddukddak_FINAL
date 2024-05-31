93352a07356e1622f7608cec7c782982a78a5aee
NIMGNUOY <ssong9214@gmail.com>
2024-05-31 17:01:27 +0900

    240531 ym 집들이상세페이지/집들이등록페이지

--- ddukddak/src/main/resources/static/js/board/myHouseBoard/myHouseBoard.js
+++ ddukddak/src/main/resources/static/js/board/myHouseBoard/myHouseBoard.js
@@ -1,7 +1,15 @@
 const myHouseDetail = document.querySelector("#myHouseDetail");
+const boardCreateBtn = document.querySelector("#boardCreateBtn");
+
 
 myHouseDetail.addEventListener("click", () => {
 
     location.href="/myHouse/detail";
 
 });
+
+boardCreateBtn.addEventListener("click", () => {
+
+    location.href="/myHouse/registMyHouse";
+
+})
