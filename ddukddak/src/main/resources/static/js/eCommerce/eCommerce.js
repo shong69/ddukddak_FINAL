ef5f5dc16ff2f4880a2da7299b9c08306f40c0e6
seungjoo1217 <rania2002@naver.com>
2024-06-04 16:59:56 +0900

    06/04 sj 쇼핑,판매자

--- ddukddak/src/main/resources/static/js/eCommerce/eCommerce.js
+++ ddukddak/src/main/resources/static/js/eCommerce/eCommerce.js
@@ -17,3 +17,37 @@ setInterval(function(){
     $('#slide2>ul').delay(3000);
     $('#slide2>ul').animate({marginLeft: "0px"})
 });
+
+
+const countDownTimer = function (id, date) {
+    var _vDate = new Date(date); // 전달 받은 일자
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distDt = _vDate - now;
+
+        var days = Math.floor(distDt / _day);
+        var hours = Math.floor((distDt % _day) / _hour);
+        var minutes = Math.floor((distDt % _hour) / _minute);
+        var seconds = Math.floor((distDt % _minute) / _second);
+
+        //document.getElementById(id).textContent = date.toLocaleString() + "까지 : ";
+        document.getElementById(id).textContent = days + '일 ';
+        document.getElementById(id).textContent += hours + '시간 ';
+        document.getElementById(id).textContent += minutes + '분 ';
+        document.getElementById(id).textContent += seconds + '초';
+    }
+
+    timer = setInterval(showRemaining, 1000);
+}
+
+var dateObj = new Date();
+dateObj.setDate(dateObj.getDate() + 1);
+
+countDownTimer('sample01', '06/08/2024');
+countDownTimer('sample02', '06/05/2024');
--- ddukddak/src/main/resources/static/js/partner/seller/product/applyProduct.js
+++ ddukddak/src/main/resources/static/js/partner/seller/product/applyProduct.js
@@ -1,7 +1,7 @@
 const bigCategory = document.querySelector("#bigCategory");
 const smallcategory = document.querySelector("#smallCategory");
 const plusButton = document.querySelector("#productPlusButton");
-const optionBox = document.querySelector("#optionBox")
+const optionBox = document.querySelector("#optionBox");
 
 let selectOption = 1;
 
@@ -249,47 +249,66 @@ function getImageFiles(e) {
   const imagePreview = document.querySelector('.image-preview');
   const docFrag = new DocumentFragment();
 
-  if ([...files].length >= 7) {
-    alert('이미지는 최대 6개 까지 업로드가 가능합니다.');
-    return;
-  }
-
   // 파일 타입 검사
   [...files].forEach(file => {
     if (!file.type.match("image/.*")) {
       alert('이미지 파일만 업로드가 가능합니다.');
       return
     }
-
-    // 파일 갯수 검사
-    if ([...files].length < 7) {
-      uploadFiles.push(file);
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const preview = createElement(e, file);
-        imagePreview.appendChild(preview);
-      };
-      reader.readAsDataURL(file);
-    }
+    uploadFiles.push(file);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const preview = createElement(e, file);
+      imagePreview.appendChild(preview);
+    };
+    reader.readAsDataURL(file);
   });
 }
 
 function createElement(e, file) {
   const li = document.createElement('li');
   const img = document.createElement('img');
+  const text = document.createElement('div');
+
+  text.innerText = '대표사진';
+  text.classList.add("main-text");
   img.setAttribute('src', e.target.result);
   img.setAttribute('data-file', file.name);
   li.appendChild(img);
+  li.appendChild(text);
 
   return li;
 }
+
 
 const realUpload = document.querySelector('.real-upload');
 const upload = document.querySelector('.upload');
 
 upload.addEventListener('click', () => realUpload.click());
 
-realUpload.addEventListener('change', getImageFiles);
+realUpload.addEventListener('change', e => {
+  getImageFiles(e);
+
+  setTimeout(function() {
+    const mainTexts = document.querySelectorAll(".main-text");
+    const mainText = document.getElementsByClassName("main-text");
+
+    mainText[0].classList.add("first-text");
+
+    mainTexts.forEach(elements => {
+      elements.addEventListener("click", e => {
+        for(let i = 0; i < mainText.length; i ++) {
+          mainText[i].classList.remove("first-text");
+        }
+        e.target.classList.add("first-text");
+      })
+    })
+
+
+
+  }, 10);
+  
+});
 
 // input file 커스텀 - 파일명 붙이기
 const fileTarget = $('.form__input--file_wrap input');
