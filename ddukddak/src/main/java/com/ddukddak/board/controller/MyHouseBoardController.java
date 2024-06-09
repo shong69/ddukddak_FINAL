630a26b4d1554f4460e71eff535cd62eea88199f
NIMGNUOY <ssong9214@gmail.com>
2024-06-09 22:38:41 +0900

    240609 sym 집들이 등록/ 집들이 게시판

--- ddukddak/src/main/java/com/ddukddak/board/controller/MyHouseBoardController.java
+++ ddukddak/src/main/java/com/ddukddak/board/controller/MyHouseBoardController.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ddukddak.board.model.dto.Board;
 import com.ddukddak.board.model.service.MyHouseBoardService;
+import com.ddukddak.member.model.dto.Member;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +30,14 @@ public class MyHouseBoardController {
 	private final MyHouseBoardService service;
 
 	@GetMapping("main")
-	public String myHouseMain() {
+	public String myHouseMain(@RequestParam("boardType") int boardType,
+							  @RequestParam(value="cp", required = false, defaultValue = "1") int cp,
+							  Model model) {
+		
+		List<Board> myHouseList = service.selectMyHouseList(boardType);
+		
+		model.addAttribute("myHouseList", myHouseList);
+		
 		return "board/myHouseBoard/myHouseBoard";
 	}
 	
@@ -43,7 +53,8 @@ public class MyHouseBoardController {
 	
 	// 집들이 게시글 등록
 	@PostMapping("registMyHouse")
-	public String registMyHouse(@RequestParam ("boardTitle") String inputTitle,
+	public String registMyHouse(@SessionAttribute ("loginMember") Member loginMember,
+								@RequestParam ("boardTitle") String inputTitle,
 								@RequestParam ("boardContent") String inputContent,
 								@RequestParam ("mainImg") String mainImgFileName,
 								@RequestParam ("images") List<MultipartFile> images,
@@ -54,6 +65,7 @@ public class MyHouseBoardController {
 		
 	    board.setBoardTitle(inputTitle);
 	    board.setBoardContent(inputContent);
+	    board.setMemberNo(loginMember.getMemberNo());
 	    
 	    
 	    log.info("images : " + images);
@@ -82,6 +94,7 @@ public class MyHouseBoardController {
 	 
 	    if(boardNo > 0) {
 	    	
+	    	// 후에 리스트, 상세페이지 다 되면 상세페이지로 넘억가게 수정
 	    	path= "/myHouse/main";
 	    	
 	    	
