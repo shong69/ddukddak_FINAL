c14c7e447a77bdfead3f914bbe16ff88e979ff1a
NIMGNUOY <ssong9214@gmail.com>
2024-06-13 17:48:21 +0900

    240613 sym 댓글

--- ddukddak/src/main/java/com/ddukddak/board/model/service/CommentServiceImpl.java
+++ ddukddak/src/main/java/com/ddukddak/board/model/service/CommentServiceImpl.java
@@ -24,4 +24,11 @@ public class CommentServiceImpl implements CommentService {
 		return mapper.select(boardNo);
 	}
 
+	// 답글 / 댓글 등록
+	@Override
+	public int insert(Comment comment) {
+		
+		return mapper.insert(comment);
+	}
+
 }
