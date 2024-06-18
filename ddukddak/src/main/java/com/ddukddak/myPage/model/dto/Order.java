c42ace292df537e82ed1f4c4542555871a15e28e
shong69 <shong7576@gmail.com>
2024-06-19 01:53:09 +0900

    6/18 sy orderList

--- ddukddak/src/main/java/com/ddukddak/myPage/model/dto/Order.java
+++ ddukddak/src/main/java/com/ddukddak/myPage/model/dto/Order.java
@@ -17,7 +17,7 @@ public class Order {
 
 	private int orderNo; //주문 번호 
 	private String orderDate; //주문 날짜
-	private String orderStatus; //주문 상태 ('결제대기', '결제완료','구매확정')
+	private String orderStatus; //주문 상태 ('결제완료','배송중', '배송완료''구매확정')
 	private String orderDelFl; // 주문 취소 여부(Y or N)
 	private String MemberNo; //주문 회원 번호
 	private String deliveryAddress; //배송 주소
