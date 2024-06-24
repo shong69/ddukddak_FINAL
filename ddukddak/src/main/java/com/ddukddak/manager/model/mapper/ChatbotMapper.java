e496d6cc0aa0c1a01334c13d5d122461104b84ec
seungjoo1217 <rania2002@naver.com>
2024-06-24 22:36:15 +0900

    06/24 sj 챗봇

--- ddukddak/src/main/java/com/ddukddak/manager/model/mapper/ChatbotMapper.java
+++ ddukddak/src/main/java/com/ddukddak/manager/model/mapper/ChatbotMapper.java
@@ -2,7 +2,11 @@ package com.ddukddak.manager.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ddukddak.manager.model.dto.Inquiry;
+
 @Mapper
 public interface ChatbotMapper {
+
+	Inquiry findInquiryByCategoryAndQuestion(String predictedCategory, String inquiry);
 
 }
--- ddukddak/src/main/java/com/ddukddak/manager/model/service/ChatbotServiceImpl.java
+++ ddukddak/src/main/java/com/ddukddak/manager/model/service/ChatbotServiceImpl.java
@@ -5,14 +5,17 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.ddukddak.manager.NLPModel;
+import com.ddukddak.manager.model.dto.Inquiry;
 import com.ddukddak.manager.model.dto.InquiryCategory;
 import com.ddukddak.manager.model.dto.RecommendAnswer;
 import com.ddukddak.manager.model.mapper.ChatbotMapper;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ChatbotServiceImpl implements ChatbotService{
 	
 	private final ChatbotMapper mapper;
@@ -22,9 +25,18 @@ public class ChatbotServiceImpl implements ChatbotService{
 	//답변 얻기
 	@Override
 	public RecommendAnswer recommendAnswer(String categoty, String inquiry, NLPModel nlpModel) {
-		String preprocessString =  nlp.categorize(inquiry);
+		String predictedCategory =  nlp.categorize(inquiry);
+		log.info("예측된 카테고리 : " + predictedCategory);
 		
-		return null;
+		Inquiry inquiryData = mapper.findInquiryByCategoryAndQuestion(predictedCategory, inquiry);
+		
+		if(inquiryData != null) {
+			return new RecommendAnswer(true,  "답변을 찾았습니다.", inquiryData.getCategoryName());
+			
+		} else {
+			return new RecommendAnswer(false,  "답변을 못찾았습니다.", inquiryData.getCategoryName());
+		}
+		
 	}
 
 }
