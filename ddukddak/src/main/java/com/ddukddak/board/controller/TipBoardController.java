1504dee76c7069b379c491e8ea966ca32679563a
seungjoo1217 <rania2002@naver.com>
2024-06-24 21:41:34 +0900

    06/24 sj 모델링 거의완성 챗시작

--- ddukddak/src/main/java/com/ddukddak/board/controller/TipBoardController.java
+++ ddukddak/src/main/java/com/ddukddak/board/controller/TipBoardController.java
@@ -297,21 +297,22 @@ public class TipBoardController {
 		board.setBoardTitle(inputBoardTitle);
 		board.setBoardContent(inputBoardContent);
 		
-		int result = myHouseService.updateMyHouse(board, images);
-		String path = null;
-		String message = null;
-		
-		if(result > 0) {
-			path = "/tip/detail/" + boardNo;
-			message = "노하우 게시글 수정이 완료되었습니다.";
-		} else {
-			path = "/tip/updateMyHouse";
-			message = "노하우 게시글 수정에 실패하였습니다.";
-		}
-		
-		ra.addFlashAttribute("message", message);
-		
-		return "redirect:" + path;
+//		int result = myHouseService.updateMyHouse(board, images);
+//		String path = null;
+//		String message = null;
+//		
+//		if(result > 0) {
+//			path = "/tip/detail/" + boardNo;
+//			message = "노하우 게시글 수정이 완료되었습니다.";
+//		} else {
+//			path = "/tip/updateMyHouse";
+//			message = "노하우 게시글 수정에 실패하였습니다.";
+//		}
+//		
+//		ra.addFlashAttribute("message", message);
+//		
+//		return "redirect:" + path;
+		return "";
 	}
 }
 
