19f37806adf9643ee66a0f81fbd73ffdbccd5aa0
NIMGNUOY <ssong9214@gmail.com>
2024-06-26 14:00:31 +0900

    240626/sym

--- ddukddak/src/main/resources/static/js/board/myHouseBoard/myHouseBoard.js
+++ ddukddak/src/main/resources/static/js/board/myHouseBoard/myHouseBoard.js
@@ -1,14 +1,6 @@
-// const myHouseDetail = document.querySelector("#myHouseDetail");
+
 const boardCreateBtn = document.querySelector("#boardCreateBtn");
 
-
-// if (myHouseDetail != null) {
-//     myHouseDetail.addEventListener("click", () => {
-    
-//         location.href="/myHouse/detail";
-    
-//     });
-// }
 
 if (boardCreateBtn != null) {
     boardCreateBtn.addEventListener("click", () => {
